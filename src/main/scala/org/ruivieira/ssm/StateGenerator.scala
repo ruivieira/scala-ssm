926ed572dc3026e395b5ff214d0c0c3bd1c7b14d
Rui Vieira <ruidevieira@googlemail.com>
2017-05-03 13:14:47 +0100

    Added support for AR(1) DGLM models.

--- src/main/scala/org/ruivieira/ssm/StateGenerator.scala
+++ src/main/scala/org/ruivieira/ssm/StateGenerator.scala
@@ -14,7 +14,7 @@
 
 package org.ruivieira.ssm
 
-import breeze.linalg.DenseVector
+import breeze.linalg.{DenseMatrix, DenseVector}
 import breeze.stats.distributions.MultivariateGaussian
 
 object StateGenerator {
@@ -42,5 +42,35 @@ object StateGenerator {
 
     // drop the state prior
     states.drop(1)
+  }
+
+  /**
+    * Generates an [[Array]] of states corresponding to an AR(1) DGLM
+    *
+    * @param nobs The number of observations to generate
+    * @param alpha First auto-regressive parameter
+    * @param beta Second auto-regressive parameter
+    * @param state0 The initial state as a [[DenseVector]]
+    * @param W The state's variance, `W > 0`
+    * @return An [[Array]] of [[DenseVector]] states
+    */
+  def ar1(nobs: Int,
+          alpha: Double,
+          beta: Double,
+          state0: DenseVector[Double],
+          W: Double): Array[DenseVector[Double]] = {
+
+    val states = Array.ofDim[DenseVector[Double]](nobs + 1)
+    val variance = DenseMatrix.eye[Double](1) * W
+
+    states(0) = state0
+    (1 to nobs).foreach(
+      t =>
+        states(t) = MultivariateGaussian(mean = states(t - 1) * beta + alpha,
+                                         covariance = variance).sample())
+
+    // drop the state prior
+    states.drop(1)
+
   }
 }
